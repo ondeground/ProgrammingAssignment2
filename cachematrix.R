f35ad04774cff60d5e17c4171e670815700faecc
jager <ondeground@gmail.com>
2015-04-25 14:51:35 -0500

    Assignment 2
    My submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+                                            ## These functions are able to cache the inverse matrix of any given matrix THAT IS INVERTIBLE.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+                                            #makeCacheMatrix creates a list containing four functions
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }                                         #first function, used to set the value of the matrix
+  get <- function() x                       #second function, used to get the value of the martix
+  setMatrix <- function(solve) m <<- solve  #third function, used to set the inverse matrix
+  getMatrix <- function() m                 #fourth function, used to get the value of the inverse matrix
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+                                            ## This function calculates the inverse Matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x,...) {
+        
+  m <- x$getMatrix()                         
+    if(!is.null(m)) {
+    message("saving time-consuming computations by getting from the cache the value of the inverse Matrix")
+    return(m)
+  }                                         #This condition checks if the inverse matrix has already been calculated. If so, it gets the value of the cache and skips calculating the inverse of the matrix.
+  data <- x$get()
+  m <- solve(data,...)
+  x$setMatrix(m)
+  m
+}                                           #Calculates the inverse of the matrix 
